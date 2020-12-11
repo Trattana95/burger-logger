4cb5dc74e10141e6b708bc5fff91688d18d19672
Thom Rattana <thomr95@gmail.com>
2020-12-11 01:47:31 -0500

    basics

---
+++ burgerDB/seeds.sql
@@ -0,0 +1,7 @@
+
+
+INSERT INTO burger (burger_name, devoured) VALUES ('Chesseburger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('PhillySteakBurger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('HotHotBurger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('DoubleDecker', true);
+
